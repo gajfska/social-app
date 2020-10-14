c7a95a33e078c2339c879ffed57abe0092c307fc
Werka <weronika.gajska@gmail.com>
2020-10-14 18:19:45 +0200

    styling main-screen component

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -16,10 +16,10 @@ export interface SocialModel {
 export class MainScreenComponent {
 
     socialMenu: SocialModel[] = [
-        {icon: '../../assets/images/screen1_content_post1_phone.png', time: '1 minute ago', author: 'by Mike | Creative Minst', description: 'Mechanical Grasshopper'},
-        {icon: '../../assets/images/screen1_content_post2_cloud.png', time: '21 minute ago', author: 'by Dash', description: 'Assistant App- Weather Module'},
-        {icon: '../../assets/images/screen1_content_post3_picture.png', time: '1 hour ago', author: 'by Zeki Ghulam', description: 'Flat & Filthy- My upcoming web agency'},
-        {icon: '../../assets/images/screen1_content_post4_logo.png', time: '2 hours ago', author: 'by Bill & Kennedy', description: 'Bamboo Branding Wall Peace- not flat'},
+        {icon: '../../assets/images/screen1_content_post1_phone.png', time: '1 minute ago ', author: 'by Mike | Creative Minst', description: 'Mechanical Grasshopper'},
+        {icon: '../../assets/images/screen1_content_post2_cloud.png', time: '21 minute ago ', author: 'by Dash', description: 'Assistant App- Weather Module'},
+        {icon: '../../assets/images/screen1_content_post3_picture.png', time: '1 hour ago ', author: 'by Zeki Ghulam', description: 'Flat & Filthy- My upcoming web agency'},
+        {icon: '../../assets/images/screen1_content_post4_logo.png', time: '2 hours ago ', author: 'by Bill & Kennedy', description: 'Bamboo Branding Wall Peace- not flat'},
     ];
 
 }
